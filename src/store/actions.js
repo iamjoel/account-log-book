8ecdf61c50abce629fa048ba592d032a453acbd4
joel <iamjoel007@gmail.com>
2018-11-16 11:02:42 +0800

    build

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,10 +2,6 @@ import * as types from './mutation-types'
 import { urls } from '@/setting'
 import axios from 'axios'
 
-export const setUser = ({ commit }, user) => {
-  commit(types.USER_INFO, user)
-}
-
 export const changeActiveType = ({ commit }, type) => {
   commit(types.CHANGE_ACTIVE_TYPE, type)
 }
